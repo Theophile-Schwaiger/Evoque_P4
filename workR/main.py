22ff5ed220d2fa0bc85714dabc05f1007e5b24e0
Theophile-Schwaiger <theophile.schwaiger@student.uclouvain.be>
2020-03-16 18:04:21 +0100

    test de changement du main

--- workR/main.py
+++ workR/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-    
+    test de changement
+
+
        Universite catholique de Louvain
        CEREM : Centre for research in mechatronics
        http://www.robotran.be  
